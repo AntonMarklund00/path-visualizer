0af9bac6f48c695b90e7821556d2f573623dcc88
Anton Marklund <anton.marklund00@gmail.com>
2022-01-02 21:28:35 +0100

    add test to ensure correct results for AStarService and DjikstraService

---
+++ src/test/java/com/marklund/path/pather/pathfinders/AStarServiceTest.java
@@ -0,0 +1,48 @@
+package com.marklund.path.pather.pathfinders;
+
+import com.marklund.path.pather.pathfinders.due.Cell;
+import com.marklund.path.pather.pathfinders.service.AStarService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+public class AStarServiceTest {
+    @Autowired
+    private AStarService<Cell> aStarService;
+
+    private Integer[][] underTest = new Integer[][] {
+            {1,1,1,1,1,1,1,1,1},
+            {1,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,1},
+            {1,2,0,0,0,0,0,3,1},
+            {1,0,0,0,0,0,0,0,1},
+            {1,1,1,1,1,1,1,1,1},
+    };
+    @Test
+    public void isValidIfTheAmountOfVisitedNodesAreCorrect() {
+        // when
+        Map<Integer, List<Integer[]>> givenResultFromService =
+                aStarService.findClosestPath(underTest, 3, 1, 3, 7);
+        int amountOfStepsTaken = givenResultFromService.get(0).size();
+
+        // then
+        assertThat(amountOfStepsTaken).isEqualTo(18);
+    }
+
+    @Test
+    public void isValidIfTheAmountOfStepsInShortestPathIsCorrect(){
+        // when
+        Map<Integer, List<Integer[]>> givenResultFromService =
+                aStarService.findClosestPath(underTest, 3, 1, 3, 7);
+        int amountOfStepsTaken = givenResultFromService.get(1).size();
+
+        // then
+        assertThat(amountOfStepsTaken).isEqualTo(5);
+    }
+}
---
+++ src/test/java/com/marklund/path/pather/pathfinders/DjikstraServiceTest.java
@@ -0,0 +1,49 @@
+package com.marklund.path.pather.pathfinders;
+
+import com.marklund.path.pather.pathfinders.due.Cell;
+import com.marklund.path.pather.pathfinders.service.DjikstraService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+public class DjikstraServiceTest {
+    @Autowired
+    private DjikstraService<Cell> djikstraService;
+
+    private Integer[][] underTest = new Integer[][] {
+            {1,1,1,1,1,1,1,1,1},
+            {1,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,1},
+            {1,2,0,0,0,0,0,3,1},
+            {1,0,0,0,0,0,0,0,1},
+            {1,1,1,1,1,1,1,1,1},
+    };
+
+    @Test
+    public void isValidIfTheAmountOfVisitedNodesAreCorrect() {
+        // when
+        Map<Integer, List<Integer[]>> givenResultFromService =
+                djikstraService.findClosestPath(underTest, 3, 1, 3, 7);
+        int amountOfStepsTaken = givenResultFromService.get(0).size();
+
+        // then
+        assertThat(amountOfStepsTaken).isEqualTo(24);
+    }
+
+    @Test
+    public void isValidIfTheAmountOfStepsInShortestPathIsCorrect(){
+        // when
+        Map<Integer, List<Integer[]>> givenResultFromService =
+                djikstraService.findClosestPath(underTest, 3, 1, 3, 7);
+        int amountOfStepsTaken = givenResultFromService.get(1).size();
+
+        // then
+        assertThat(amountOfStepsTaken).isEqualTo(5);
+    }
+}
