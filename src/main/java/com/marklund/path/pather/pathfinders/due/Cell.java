aa0ff609a338e05dd8b0ada46d7c8fcd51ee33ef
Anton Marklund <anton.marklund00@gmail.com>
2022-01-06 17:31:17 +0100

    remove name property for cells

--- src/main/java/com/marklund/path/pather/pathfinders/due/Cell.java
+++ src/main/java/com/marklund/path/pather/pathfinders/due/Cell.java
@@ -8,11 +8,9 @@ public abstract class Cell {
     protected double heuristicDistance;
     protected Cell parent;
     protected boolean isVisited;
-    protected String name;
     protected boolean isWall;
 
     public Cell(int x, int y) {
-        this.name = null;
         this.x = x;
         this.y = y;
     }
@@ -67,13 +65,5 @@ public abstract class Cell {
 
     public double getCombinedDistance() {
         return getHeuristicDistance() + getDistanceFromStart();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
